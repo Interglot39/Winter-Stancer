e84bb8a481123fd1aafa87ec5004e268d1e43c4b
Winterglot <jaimedemora@hotmail.com>
2021-10-18 02:52:20 +0200

    script

---
+++ winter_wstancer/client/client.lua
@@ -0,0 +1,104 @@
+local tuning = false
+local checked = false
+local width = {}
+local wheelBones = {
+	[0] = "wheel_lf",
+	[1] = "wheel_rf",
+	[2] = "wheel_lr",
+	[3] = "wheel_rr"
+}
+
+Citizen.CreateThread(function()
+	DecorRegister('wheelLF', 1)
+	DecorRegister('wheelRF', 1)
+	DecorRegister('wheelLR', 1)
+	DecorRegister('wheelRR', 1)
+end)
+
+
+Citizen.CreateThread(function()
+	local pId = nil
+	local pIdVehicle = nil
+	local pIdVehicleClass = nil
+	while true do
+		pId = PlayerPedId()
+		Citizen.Wait(200)
+		if IsPedInAnyVehicle(pId, false) then
+			pIdVehicle = GetVehiclePedIsIn(pId, false)
+			pIdVehicleClass = GetVehicleClass(pIdVehicle)
+			if pIdVehicleClass ~= 8 and pIdVehicleClass ~= 13 and pIdVehicleClass ~= 14 and pIdVehicleClass ~= 15 and pIdVehicleClass ~= 16 and pIdVehicleClass ~= 21 and not tuning and not checked then
+				width = {DecorGetFloat(pIdVehicle, "wheelLF"), DecorGetFloat(pIdVehicle, "wheelRF"), DecorGetFloat(pIdVehicle, "wheelLR"), DecorGetFloat(pIdVehicle, "wheelRR")}
+				updateWheels()
+				checked = true
+			end
+		else
+			checked = false
+		end
+	end
+end)
+
+RegisterCommand("wheel", function()
+	modifyWheels()
+end, false)
+
+function modifyWheels()
+	local pIdVehicle = GetVehiclePedIsIn(PlayerPedId(), false)
+	width = {GetVehicleWheelXOffset(pIdVehicle, 0), GetVehicleWheelXOffset(pIdVehicle, 1), GetVehicleWheelXOffset(pIdVehicle, 2), GetVehicleWheelXOffset(pIdVehicle, 3)}
+	local modified = false
+	local wheel = 0
+	local wheelPos = 0
+	FreezeEntityPosition(pIdVehicle, true) -- Freeze vehicle at start so it does not reset wheels
+	while not IsControlJustReleased(0, 38) do  -- Until you do not press the desired key Check Config for more it does not stop
+		tuning = true
+		SetVehicleEngineOn(pIdVehicle, false, true, false)  -- Turn engine of the vehicle off
+		if IsControlJustPressed(0, 172) then  -- Advance on wheel list
+			if wheel < 3 then
+				wheel = wheel + 1
+			end 
+		elseif IsControlJustPressed(0, 173) then  -- Minus on wheel list 
+			if wheel > 0 then
+				wheel = wheel - 1
+			end 	
+		elseif IsControlJustPressed(0, 174) then  -- Push wheel left
+			width[wheel + 1] = width[wheel + 1] - 0.01
+			modified = true
+		elseif IsControlJustPressed(0, 175) then  -- Push wheel right
+			width[wheel + 1] = width[wheel + 1] + 0.01
+			modified = true
+		end
+		wheelPos = GetEntityBonePosition_2(pIdVehicle, GetEntityBoneIndexByName(pIdVehicle, wheelBones[wheel]))  -- Gets coords for later on showing a marker
+		DrawMarker(0, wheelPos.x, wheelPos.y, wheelPos.z + 1.0, 0, 0, 0, 0, 0, 0, 0.55, 0.55, 0.55, 35, 255, 0, 255, false, false) -- Draw a marker on top of the wheel to know the wheel we are working on.
+		if modified then 
+			SetVehicleWheelXOffset(pIdVehicle, wheel, width[wheel + 1])
+		end 
+		modified = false
+		Citizen.Wait(0)
+	end
+	tuning = false
+	DecorSetFloat(pIdVehicle, "wheelLF", width[1]) -- Setting the data we modified
+	DecorSetFloat(pIdVehicle, "wheelRF", width[2]) -- Setting the data we modified
+	DecorSetFloat(pIdVehicle, "wheelLR", width[3]) -- Setting the data we modified
+	DecorSetFloat(pIdVehicle, "wheelRR", width[4]) -- Setting the data we modified
+	SetVehicleEngineOn(pIdVehicle, true, true, false) -- Engine on again
+	FreezeEntityPosition(pIdVehicle, false) -- Unfreeze
+end
+
+function updateWheels() -- If you dont constantly apply the properties, they reset to normal
+	local count = 4
+	local pIdVehicle = GetVehiclePedIsIn(PlayerPedId(), false)
+	while IsPedInAnyVehicle(PlayerPedId(), false) do
+		count = 4
+		pIdVehicle = GetVehiclePedIsIn(PlayerPedId(), false)
+		for k,v in pairs(width) do
+			print("width[k]", width[k])
+			if width[k] ~= 0.0 then
+				SetVehicleWheelXOffset(pIdVehicle, k - 1, width[k])
+				count = count - 1
+			end
+		end
+		if count == 4 then
+			break
+		end
+		Citizen.Wait(0)
+	end
+end
---
+++ winter_wstancer/fxmanifest.lua
@@ -0,0 +1,13 @@
+fx_version 'bodacious'
+game 'gta5'
+
+ui_page 'html/index.html'
+
+
+client_scripts {'client/*.lua'}
+
+files {
+	'html/index.html',
+	'html/jquery.js',
+	'html/init.js',
+}
